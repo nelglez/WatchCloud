4f80d8926c649391ef85d78bb1b1b204b24f766e
Bogdan Dovgopol <bogdan.dev>
2019-05-10 19:34:24 +0200

    initial commit

---
+++ WatchCloud/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchCloud
+//
+//  Created by Bogdan Dovgopol on 10/5/19.
+//  Copyright © 2019 Bogdan Dovgopol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
